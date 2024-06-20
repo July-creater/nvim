6a9ec7800a4c3dd57e80b8dc2bfed16725fcf69d
wq <wqjuly@qq.com>
2024-06-20 20:20:16 +0800

    [feature]: update nvim

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -4,12 +4,12 @@ return {
   version = "*",
   config = function()
     require("toggleterm").setup({
-      size = 20, -- 设置终端窗口的高度
+      size = 30, -- 设置终端窗口的高度
       open_mapping = [[<c-\>]], -- 设置打开终端的快捷键
       hide_numbers = true, -- 隐藏行号
       shade_filetypes = {},
       shade_terminals = true, -- 终端窗口阴影效果
-      shading_factor = 2, -- 阴影程度
+      shading_factor = 1, -- 阴影程度
       start_in_insert = true, -- 启动时进入插入模式
       insert_mappings = true, -- 允许在插入模式下使用打开终端的快捷键
       persist_size = true, -- 记住终端窗口的大小
@@ -20,10 +20,10 @@ return {
         border = "curved",
         winblend = 0,
         width = function()
-          return math.floor(vim.o.columns * 0.4)
+          return math.floor(vim.o.columns * 0.5)
         end,
         height = function()
-          return math.floor(vim.o.lines * 0.3)
+          return math.floor(vim.o.lines * 0.4)
         end,
         highlights = {
           border = "Normal",
